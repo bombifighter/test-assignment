281aad27d7dca76eff751f441eb18a494241553a
benedek99 <szabobenedek99@gmail.com>
2021-04-27 18:29:02 +0200

    deleted before, after

--- src/test/java/com/unideb/stepdef/StepDefinitionYTLoginNeeded.java
+++ src/test/java/com/unideb/stepdef/StepDefinitionYTLoginNeeded.java
@@ -62,36 +62,4 @@ public class StepDefinitionYTLoginNeeded extends TestRunner {
         assertEquals(driver.getCurrentUrl(), url);
     }
 
-
-
-    @Before
-    public void before(Scenario scenario) {
-        System.out.println("------------------------------");
-        System.out.println("Starting - " + scenario.getName());
-        System.out.println("------------------------------");
-    }
-
-    @After
-    public void after(Scenario scenario) {
-        System.out.println("------------------------------");
-        System.out.println(scenario.getName() + " Status - " + scenario.getStatus());
-        System.out.println("------------------------------");
-        if (driver != null) {
-            driver.close();
-        }
-    }
-
-    @BeforeStep
-    public void beforeStep() {
-        System.out.println("------------------------------");
-        System.out.println("before step");
-        System.out.println("------------------------------");
-    }
-
-    @AfterStep
-    public void afterStep() {
-        System.out.println("------------------------------");
-        System.out.println("after step");
-        System.out.println("------------------------------");
-    }
 }
