00612c97c2e6fb84605203df457ad74bf94206e5
adampallagi <adampp9@gmail.com>
2021-04-27 19:25:22 +0200

    Updated YT Login fail

--- src/test/java/com/unideb/stepdef/StepDefinitionYTLoginFail.java
+++ src/test/java/com/unideb/stepdef/StepDefinitionYTLoginFail.java
@@ -8,11 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.unideb.TestRunner;
 
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
-import io.cucumber.java.Scenario;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -47,61 +43,35 @@ public class StepDefinitionYTLoginFail extends TestRunner {
 
     @When("^I click the login button$")
     public void clickLogin() {
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"buttons\"]/ytd-button-renderer/a"));
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        WebElement loginButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"buttons\"]/ytd-button-renderer/a")));
+        //WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"buttons\"]/ytd-button-renderer/a"));
         loginButton.click();
     }
 
     @When("I write {string} to the username field")
     public void writeWrongUsername(String wrongusername) {
-        WebElement usernameField = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input"));
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")));
+        //WebElement usernameField = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input"));
         usernameField.sendKeys(wrongusername);
     }
 
     @When("^I click next button$")
     public void clickNext() {
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[2]"));
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        WebElement loginButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[2]")));
+        //WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[2]"));
         loginButton.click();
     }
 
     @Then("The {string} should be {string}")
     public void getResponseText(String responsetext, String text) {
         WebDriverWait wait = new WebDriverWait(driver,5);
-
-        WebElement barmi = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(responsetext)));
-        String response = barmi.getText();
+        WebElement resp = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(responsetext)));
+        String response = resp.getText();
         System.out.println(response);
         assertEquals(response, text);
-    }
-
-    @Before
-    public void before(Scenario scenario) {
-        System.out.println("------------------------------");
-        System.out.println("Starting - " + scenario.getName());
-        System.out.println("------------------------------");
-    }
-
-    @After
-    public void after(Scenario scenario) {
-        System.out.println("------------------------------");
-        System.out.println(scenario.getName() + " Status - " + scenario.getStatus());
-        System.out.println("------------------------------");
-        if (driver != null) {
-            driver.close();
-        }
-    }
-
-    @BeforeStep
-    public void beforeStep() {
-        System.out.println("------------------------------");
-        System.out.println("before step");
-        System.out.println("------------------------------");
-    }
-
-    @AfterStep
-    public void afterStep() {
-        System.out.println("------------------------------");
-        System.out.println("after step");
-        System.out.println("------------------------------");
     }
 
 }
