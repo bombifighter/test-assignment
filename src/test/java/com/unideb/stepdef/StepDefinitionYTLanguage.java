abb67de8a9c72d8f2631eeeb045ffd1b05e59b00
bombifighter <43323146+bombifighter@users.noreply.github.com>
2021-04-28 20:59:50 +0200

    Actions (#2)
    * Create maven.yml
    
    * Update StepDefinitionYTLanguage.java
    
    * Update maven.yml
    
    * Update StepDefinitionYTLanguage.java
    
    * Update StepDefinitionYTLanguage.java
    
    * Update StepDefinitionYTLanguage.java
    
    * Update StepDefinitionYTLanguage.java
    
    * Update maven.yml

--- src/test/java/com/unideb/stepdef/StepDefinitionYTLanguage.java
+++ src/test/java/com/unideb/stepdef/StepDefinitionYTLanguage.java
@@ -8,6 +8,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -24,7 +25,11 @@ public class StepDefinitionYTLanguage extends TestRunner {
     @Given("^I have opened the browser for language test$")
     public void openBrowserYTTranscript() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--remote-debugging-port=9222");
+        driver = new ChromeDriver(chromeOptions);
     }
 
     @When("^I maximize the window for language test$")
